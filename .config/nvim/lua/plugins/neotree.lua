0c0e2259d91d70bb3ffd791ab483edad20673958
Mark Arkhipychev <hackyourrself@proton.me>
2025-12-24 22:18:28 +0100

    [Nvim] Update neotree config

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -5,13 +5,37 @@ return {
       opts.filesystem = opts.filesystem or {}
       opts.filesystem.filtered_items = opts.filesystem.filtered_items or {}
 
+      -- Only show icon and name (disable size, type, last_modified columns)
+      opts.filesystem.renderers = {
+        directory = {
+          { "icon" },
+          { "current_filter" },
+          { "name" },
+        },
+        file = {
+          { "icon" },
+          { "name" },
+          { "git_status", highlight = "NeoTreeDimText" },
+        },
+      }
+
       -- Make hidden files (dotfiles) visible
       opts.filesystem.filtered_items.hide_dotfiles = false
       opts.filesystem.filtered_items.visible = true
 
+      -- Disable auto-following the current file (useful in large monorepos)
+      opts.filesystem.follow_current_file = { enabled = false }
+
       -- Disable the source selector tabs (File/Bufs/Git)
       opts.source_selector = opts.source_selector or {}
       opts.source_selector.winbar = false
+
+      -- Add zz mapping to center the view
+      opts.window = opts.window or {}
+      opts.window.mappings = opts.window.mappings or {}
+      opts.window.mappings["zz"] = function()
+        vim.cmd("normal! zz")
+      end
 
       return opts
     end,
